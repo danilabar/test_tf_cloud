a477f20308de032649bebba21e82f7ff81d89418
Danila Salovskiy <danilabar@mail.ru>
2023-01-22 14:32:44 +0300

    fix keygen

--- terraform/ssh_keygen.tf
+++ terraform/ssh_keygen.tf
@@ -3,10 +3,6 @@ resource "null_resource" "ssh_keygen" {
     command = "ssh-keygen -t rsa -f ~/.ssh/id_rsa -C user@terraform -N ''"
   }
 
-  depends_on = [
-    local_file.inventory
-  ]
-
   triggers = {
       always_run = "${timestamp()}"
   }
