8ec87bff27b1908d290595c2863e45688556fa4e
Danila Salovskiy <danilabar@mail.ru>
2023-01-22 22:34:50 +0300

    fix add cluster

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "yandex" {
  cloud_id  = var.yandex_cloud_id
  folder_id = var.yandex_folder_id
- zone      = var.a-zones[count.index]
+ zone      = var.a-zones[0]
  service_account_key_file = var.service_account_id
 }
